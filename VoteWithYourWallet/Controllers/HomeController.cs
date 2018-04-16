a3e8f1f2dc55924a54ae321cec250fcb04cfa97a
celebren <adonnc@gmail.com>
2018-04-16 23:05:01 +0100

    End of day. Finished database delete and edit

--- VoteWithYourWallet/Controllers/HomeController.cs
+++ VoteWithYourWallet/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
@@ -25,23 +26,24 @@ namespace VoteWithYourWallet.Controllers {
             return View();
         }
 
+        // Create method
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Start([Bind(Include = "Title, Text")] Petition petition) {
             petition.Signatures = 0;
-   
+
             // validation; inform user that both fields must be filled
             if (petition.Title == null) {
                 ModelState.AddModelError("",
                     "Please provide a title for your cause");
-            } else if (petition.Text == null) {
+            }
+            else if (petition.Text == null) {
                 ModelState.AddModelError("",
                     "Please provide a description for your cause");
             }
 
             try {
                 if (ModelState.IsValid) {
-                    
                     // add object to database
                     _db.Petitions.Add(petition);
                     _db.SaveChanges();
@@ -55,7 +57,37 @@ namespace VoteWithYourWallet.Controllers {
             }
 
             return View(petition);
-           
+        }
+
+        // View cause controller
+        public ActionResult ViewCause(int id) {
+            Petition petitionToView = _db.Petitions.Find(id);
+            ViewBag.Message = petitionToView.Title;
+
+            return View(petitionToView);
+        }
+
+        // Increment signatures
+        [HttpPost, ActionName("ViewCause")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ViewCauseIncremented(int id) {
+            Petition petition = _db.Petitions.Find(id);
+            petition.Signatures += 1;
+            try {
+                if (ModelState.IsValid) {
+                    
+                    _db.Entry(petition).State = EntityState.Modified;
+                    _db.SaveChanges();
+                    Debug.WriteLine("petition id: " + petition.Id);
+                    return RedirectToAction("ViewCause/" + petition.Id);
+                }
+            }
+            catch (DataException) {
+                ModelState.AddModelError("",
+                    "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            return View(petition);
         }
 
 
@@ -66,6 +98,24 @@ namespace VoteWithYourWallet.Controllers {
             ViewData.Model = _db.Petitions.ToList();
 
             return View();
+        }
+
+
+        // /Home/Delete
+        public ActionResult Delete(int id) {
+            Petition petitionToBeDeleted = _db.Petitions.Find(id);
+            return View(petitionToBeDeleted);
+        }
+
+        // Delete method
+        // POST: /Home/Delete/
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id) {
+            Petition petition = _db.Petitions.Find(id);
+            _db.Petitions.Remove(petition);
+            _db.SaveChanges();
+            return RedirectToAction("Browse");
         }
 
         public ActionResult SampleCause() {
